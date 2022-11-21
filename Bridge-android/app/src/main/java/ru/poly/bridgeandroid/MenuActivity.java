e6ea4aba44f4be82d4a677d019c8554696099ea9
Ann <annk1211>
2022-11-22 01:56:29 +0300

    Added list view, fixed some button shapes

--- Bridge-android/app/src/main/java/ru/poly/bridgeandroid/MenuActivity.java
+++ Bridge-android/app/src/main/java/ru/poly/bridgeandroid/MenuActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -16,6 +18,19 @@ public class MenuActivity extends AppCompatActivity {
 
         final Button create_game_btn = findViewById(R.id.create_btn);
         final Button join_game_btn = findViewById(R.id.join_btn);
+
+        ListView listView = findViewById(R.id.listView);
+
+        final String[] catNames = new String[] {
+                "Рыжик", "Барсик", "Мурзик", "Мурка", "Васька",
+                "Томасина", "Кристина", "Пушок", "Дымка", "Кузя",
+                "Китти", "Масяня", "Симба"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, catNames);
+
+        listView.setAdapter(adapter);
 
         create_game_btn.setOnClickListener(new View.OnClickListener() {
             @Override
